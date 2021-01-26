9442b4fdbb9d1452eb0b9e49439f89fcfef88acf
onlyyy0801 <only0801yy@163.com>
2021-01-26 14:00:02 +0800

    01-26-红烧肉组-试卷模块-第一次提交

--- js/editPaper.js
+++ js/editPaper.js
@@ -1,7 +1,7 @@
 let JsonUserMsg = localStorage.getItem("UserMsg");
 let UserMsg = JSON.parse(JsonUserMsg);
-
-
+let editPaperTable = $('#hsr-editPaper-table');
+// 入口函数
 $(function (){
 // 加载试卷参数 -- 包括 题目、时长
     loadPaperParam();
@@ -9,7 +9,7 @@ $(function (){
     loadTestCon();
 });
 function reloadTestCon() {
-    $("#hsr-editPaper-table").bootstrapTable("refresh");
+    editPaperTable.bootstrapTable("refresh");
 }
 // 加载分类参数
 function loadClassifyParam() {
@@ -44,16 +44,15 @@ function loadPaperParam() {
         contentType: "application/json",
         dataType: "json",
         success: function (result) {
-            if(result) reloadTestCon();
-            else {
-
-            }
+            $('#hsr-editPaper-value').val(result.pName);
+            $('#hsr-editPaper-time').val(result.pTime);
         }
     });
 }
 // 加载试卷试题内容
 function loadTestCon() {
-    $('#hsr-editPaper-table').bootstrapTable({
+    editPaperTable.html('');
+    editPaperTable.bootstrapTable({
         url: pathOl + "showPaperTest",
         method: "post",
         dataType: "json",
@@ -92,17 +91,97 @@ function loadTestCon() {
                 // halign:'center',//垂直居中
                 // field: 'xxx',
                 formatter:function (value,row,index) {
-                    let changeStatus = '<button onclick="changeStatus(\'' + row.pId + '\',\'' + row.pStatus+'\')">'+status+'</button>';
-                    let showCode = '<button onclick="showCode(\'' + row.pCode + '\')">考试码</button>';
-                    let editPaper = '<button onclick="editPaper(\''+row.pId+'\')">编辑</button>';
-                    let deletePaper = '<button onclick="deletePaper(\''+row.pId+'\',\''+UserMsg.uId+'\')">删除</button>';
-                    return changeStatus + showCode + editPaper + deletePaper;
+                    return '<button onclick="delPaperTest(\'' + row.tId + '\',\'' + row.tType + '\')">删除</button>';
                 }
             }
         ]
     });
 }
-// 修改试卷 标题及时长
+// 方法：添加试题到试卷中
+function addTestToPaper(tId,tType){
+    let data = {
+        pId: $('input[name=pId]').val(),
+        tId: tId,
+        tType: tType
+    }
+    $.ajax({
+        url: pathOl + "addPaperTest",
+        type: "post",
+        data: JSON.stringify(data),
+        contentType: "application/json",
+        dataType: "json",
+        success: function (result) {
+            loadPaperParam();
+        }
+    });
+}
+// 方法：添加试题到试卷中
+function delPaperTest(tId,tType){
+    let data = {
+        pId: $('input[name=pId]').val(),
+        tId: tId,
+        tType: tType
+    }
+    $.ajax({
+        url: pathOl + "delPaperTest",
+        type: "post",
+        data: JSON.stringify(data),
+        contentType: "application/json",
+        dataType: "json",
+        success: function (result) {
+            reloadTestCon();
+        }
+    });
+}
+// 方法：加载可以添加的试题
+function loadAddibleTest() {
+    editPaperTable.html('');
+    editPaperTable.bootstrapTable({
+        url: pathOl + "showAddibleTest",
+        method: "post",
+        dataType: "json",
+        striped:true,       //设置表格隔行换色效果
+        pageNumber:1,       //初始化加载第一页
+        pagination:true,    //是否分页
+        sidePagination:'server',  //服务器端client不分页 server分页
+        pageSize:6,         //单页记录数  告知前端使用者  每页显示多少个
+        queryParams:function(params){
+            let data={
+                uId:UserMsg.uId,
+                pId: $('input[name = pId]').val(),
+                offset:params.offset,   //SQL语句启示索引
+                pageNumber: params.limit    //每页显示数量
+            }
+            return JSON.stringify(data);
+        },
+        columns:[
+            {
+                title:'序号',
+                align:'center',//水平居中
+                // halign:'center',//垂直居中
+                formatter:function (value,row,index) {
+                    return index+1;
+                }
+            },
+            {
+                title: '题目',
+                align:'center',//水平居中
+                // halign:'center',//垂直居中
+                field: 'tName'
+            },
+            {
+                title: '操作',
+                align:'center',//水平居中
+                // halign:'center',//垂直居中
+                // field: 'xxx',
+                formatter:function (value,row,index) {
+                    return '<button onclick="addTestToPaper(\'' + row.tId + '\',\'' + row.tType + '\')">添加</button>';
+                }
+            }
+        ]
+    });
+}
+// 点击事件：修改试卷 标题及时长
 $('#hsr-editPaper-edit').click(function () {
     let data = {
         pId: $('input[name = pId]').val(),
@@ -120,13 +199,20 @@ $('#hsr-editPaper-edit').click(function () {
         }
     });
 });
-// 显示所有可以添加的试题
+// 点击事件：显示所有可以添加的试题
 $('#hsr-editPaper-addTestBtn').click(function () {
-    let editPaperTable = $('#hsr-editPaper-table');
+    $('#hsr-editPaper-addTestBtn').hide();
+    $('#hsr-editPaper-showTestBtn').show();
+    // 加载筛选模块
     loadClassifyParam();
     $('#hsr-editPaper-filter').show();
-    editPaperTable.html('');
-    editPaperTable.bootstrapTable({
-
-    });
+    // 加载可添加的试题
+    loadAddibleTest();
+});
+// 点击事件：显示试卷中已添加的试题
+$('#hsr-editPaper-showTestBtn').click(function () {
+    $('#hsr-editPaper-addTestBtn').show();
+    $('#hsr-editPaper-showTestBtn').hide();
+    $('#hsr-editPaper-filter').hide();
+    loadTestCon();
 });
