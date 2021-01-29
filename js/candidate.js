b11ae8785679db58ebbb45e5a8917a92184adf94
onlyyy0801 <only0801yy@163.com>
2021-01-29 09:25:30 +0800

    01.29-红烧肉组-考试模块-前端-第一次提交

--- js/candidate.js
+++ js/candidate.js
@@ -15,6 +15,7 @@ phone.on('blur',function () {
     } else changeRed(phone);
 });
 cName.on('blur',function () {
+    console.log(cName.val().length);
     if(cName.val().length ===2 || cName.val().length ===3) {
         changeBlue(cName);
         markCname = true;
@@ -31,12 +32,12 @@ examStart.on('click',function () {
 
     if(markPhone && markCname && markPcode){
         let data = {
-            uAcc: phone.val(),
-            uPwd: cName.val(),
-            uName: pCode.val()
+            rPhone: phone.val(),
+            rName: cName.val(),
+            pCode: pCode.val()
         };
         $.ajax({
-            url: pathOl + 'sigin',
+            url: pathOl + 'addRecord',
             type: 'post',
             data: JSON.stringify(data),
             contentType: 'application/json',
@@ -55,9 +56,9 @@ examStart.on('click',function () {
                         type:2,//可传入的值有：0（信息框，默认）1（页面层）2（iframe层）3（加载层）4（tips层）。
                         title:'添加试题',
                         maxmin:false,
-                        shadeClose:true,
+                        shadeClose:false,
                         area:['70%','70%'],//弹出层的宽高
-                        content:'con-pages/beforeExam.html',//设置弹出层打开的页面
+                        content:'beforeExam.html',//设置弹出层打开的页面
                         closeBtn: 0          //没有关闭按钮X
                         //弹出层页面成功打开后，的设置       加载子页面 渲染子页面 load后端数据 弹出 success 展示成功
                         // success:function(layero,index){
